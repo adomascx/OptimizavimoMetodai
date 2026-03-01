fd4c5d97e39fd71ad609c0c1772f865afbd9c985
adomascx <adlukosev2@gmail.com>
2026-03-01 21:16:29 +0200

    1LD: implement 3 main algorithms, add function + derivatives per requirements, Git essentils and `docx` report

--- 1LD/main.go
+++ 1LD/main.go
@@ -1,7 +1,146 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+type tiksloFunkcija func(x float64) float64
+
+// mano studento skaitmenys
+var A float64 = 3
+var B float64 = 9
+
+// Tikslo funkcija + jos 1ojo ir 2ojo laipsnio isvestines
+func funkcija(x float64) (y float64) {
+	return math.Pow(x*x-A, 2) / (B - 1)
+}
+
+func isvestine_1(x float64) (y float64) {
+	return (x * 4 * (x*x - A)) / (B - 1)
+}
+
+func isvestine_2(x float64) (y float64) {
+	return (x*x*12 - A*4) / (B - 1)
+}
+
+// Algoritmu implementacijos
+func intervaloDalijimoPusiauAlgo(f tiksloFunkcija, l, r, epsilon float64, maxIter int) (xMin, fMin float64, iteracijuSk, funkcijosSkaiciavimuSk int, err error) {
+
+	funkcijosSkaiciavimuSk = 0
+
+	for i := 0; i < maxIter; i++ {
+
+		if (r - l) <= epsilon {
+			x := (l + r) / 2
+			return x, f(x), i, funkcijosSkaiciavimuSk + 1, nil
+		}
+
+		L := r - l
+		xm := (l + r) / 2
+		x1 := l + L/4
+		x2 := r - L/4
+
+		if f(x1) < f(xm) {
+			r = xm
+			funkcijosSkaiciavimuSk += 2
+
+		} else if f(x2) < f(xm) {
+			l = xm
+			funkcijosSkaiciavimuSk += 3
+
+		} else {
+			l = x1
+			r = x2
+			funkcijosSkaiciavimuSk += 3
+
+		}
+
+	}
+
+	x := (l + r) / 2
+
+	return x, f(x), maxIter, funkcijosSkaiciavimuSk + 1, fmt.Errorf("pasiektas maksimalus iteracijų skaičius.")
+
+}
+
+func auksinioPjuvioAlgo(f tiksloFunkcija, l, r, epsilon float64, maxIter int) (xMin, fMin float64, iteracijuNr, funkcijosSkaiciavimuSk int, err error) {
+
+	funkcijosSkaiciavimuSk = 0
+
+	tau := (math.Sqrt(5.0) - 1.0) / 2.0 // ~0.618
+
+	x1 := r - tau*(r-l)
+	x2 := l + tau*(r-l)
+
+	for i := 0; i < maxIter; i++ {
+
+		if (r - l) <= epsilon {
+			x := 0.5 * (l + r)
+			return x, f(x), i, funkcijosSkaiciavimuSk + 1, nil
+		}
+
+		if f(x2) < f(x1) {
+			l = x1
+			x1 = x2
+			x2 = l + tau*(r-l)
+
+		} else {
+			r = x2
+			x2 = x1
+			x1 = r - tau*(r-l)
+
+		}
+		funkcijosSkaiciavimuSk += 2
+	}
+
+	x := 0.5 * (l + r)
+	return x, f(x), maxIter, funkcijosSkaiciavimuSk + 1, fmt.Errorf("pasiektas maksimalus iteracijų skaičius.")
+}
+
+func niutonoAlgo(f, df, d2f tiksloFunkcija, x0, epsilon float64, maxIter int) (xMin, fMin float64, iteracijuNr, funkcijosSkaiciavimuSk int, err error) {
+
+	funkcijosSkaiciavimuSk = 0
+
+	x := x0
+
+	for i := 0; i < maxIter; i++ {
+
+		if math.Abs(df(x)) <= epsilon {
+			return x, f(x), i, funkcijosSkaiciavimuSk, nil
+		}
+
+		xNext := x - df(x)/d2f(x)
+		funkcijosSkaiciavimuSk += 2
+
+		if math.Abs(xNext-x) <= epsilon {
+			x = xNext
+			return x, f(x), i + 1, funkcijosSkaiciavimuSk, nil
+		}
+		x = xNext
+	}
+
+	return x, f(x), maxIter, funkcijosSkaiciavimuSk, fmt.Errorf("pasiektas maksimalus iteracijų skaičius.")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+
+	xMin, fMin, iter, funk, err := intervaloDalijimoPusiauAlgo(funkcija, 1, 10, 1e-4, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n**Intervalo Dalijimo Algoritmas**\nxMin: %v\nfMin: %.1e\niteraciju: %v\nfunkcijos skaiciavimu: %v\n", xMin, fMin, iter, funk)
+
+	xMin, fMin, iter, funk, err = auksinioPjuvioAlgo(funkcija, 1, 10, 1e-4, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n**Auksinio Pjuvio Algoritmas**\nxMin: %v\nfMin: %.1e\niteraciju: %v\nfunkcijos skaiciavimu: %v\n", xMin, fMin, iter, funk)
+
+	xMin, fMin, iter, funk, err = niutonoAlgo(funkcija, isvestine_1, isvestine_2, 5, 1e-4, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n**Niutono Algoritmas**\nxMin: %v\nfMin: %.1e\niteraciju: %v\nfunkcijos skaiciavimu: %v\n", xMin, fMin, iter, funk)
 }
---
+++ README.md
@@ -0,0 +1,24 @@
+# Optimizavimo Metodų Laboratoriniai Darbai
+
+Šis repo yra skirtas Optimizavimo Metodų laboratoriniams darbams saugoti.
+Darbas atliktas GO programavimo kalba, kiekvieno LD implementaciją įdedant į atskirą direktoriją.
+
+## Turinys
+
+- [Optimizavimo Metodų Laboratoriniai Darbai](#optimizavimo-metodų-laboratoriniai-darbai)
+  - [Turinys](#turinys)
+  - [Laboratoriniai darbai](#laboratoriniai-darbai)
+    - [1LD - Vienmatis optimizavimas](#1ld---vienmatis-optimizavimas)
+      - [Užduotis](#užduotis)
+
+## Laboratoriniai darbai
+
+### 1LD - Vienmatis optimizavimas
+
+#### Užduotis
+
+1. Suprogramuokite vienmačio optimizavimo intervalo dalijimo pusiau, auksinio pjūvio ir Niutono metodo algoritmus.
+2. Aprašykite tikslo funkciją `f(x) = (x2−a)2/b−1`, čia `a` ir `b` – studento knygelės numerio `2*1**ab` skaitmenys. Jei `b` yra 0, susumuokite visus kortelės numerio skaitmenis, skaičiuokite rezultato skaitmenų sumą ir taip darykite tol, kol gausite vienženklį skaičių, jį ir imkite kaip `b`.
+3. Minimizuokite šią funkciją intervalo dalijimo pusiau ir auksinio pjūvio metodais intervale `[0,10]` iki tikslumo `10−4` bei Niutono metodu nuo `x0 = 5` kol žingsnio ilgis didesnis už `10−4`.
+4. Palyginkite rezultatus: gauti sprendiniai, rastas funkcijos minimumo įvertis, atliktų žingsnių ir funkcijų skaičiavimų skaičius.
+5. Vizualizuokite tikslo funkciją ir bandymo taškus.
